2ad80cbfb5b53a0fb7da71a3aee9fec15cddbbc4
Git Hub Training <alooripr@gmail.com>
2024-10-03 16:49:40 -0400

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,8 +10,8 @@ terraform {
     bucket         = "my-ews-baket143"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files123"
-    encrypt        = false
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
   }
 }
 
