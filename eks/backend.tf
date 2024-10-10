32bef13fef9cf4c20279581b3ca9043ace890e19
Git Hub Training <alooripr@gmail.com>
2024-10-10 15:00:41 -0400

    Update backend.tf
    updated s3 bucket name

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket143"
+    bucket         = "my-aws-basket143"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
